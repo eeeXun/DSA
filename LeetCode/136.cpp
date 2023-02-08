85c10b54ca1da1bc60a14b131120b4f5159cd321
eeeXun <sdes96303@gmail.com>
2023-02-08 22:31:46 +0800

    LeetCode: 136
    Bad perf, use XOR (Who can think of this method? Genius)

---
+++ LeetCode/136.cpp
@@ -0,0 +1,23 @@
+#include <map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums)
+    {
+        map<int, int> data;
+        int result;
+        for (auto i : nums) {
+            if (data.find(i) == data.end())
+                data[i] = 1;
+            else
+                data[i]++;
+        }
+        for (auto it : data) {
+            if (it.second == 1)
+                result = it.first;
+        }
+        return result;
+    }
+};

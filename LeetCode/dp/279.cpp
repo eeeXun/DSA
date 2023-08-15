1d7d281f56a4401c5dd4a090fdfe41da6a263624
eeeXun <sdes96303@gmail.com>
2023-08-15 20:28:34 +0800

    LeetCode: rewrite 279

--- LeetCode/dp/279.cpp
+++ LeetCode/dp/279.cpp
@@ -1,21 +1,16 @@
 class Solution {
-private:
-    vector<int> sq_num;
-
 public:
-    Solution()
-    {
-        for (int i = 1; i < 100; i++) {
-            sq_num.push_back(i * i);
-        }
-    }
     int numSquares(int n)
     {
+        int tmp;
         vector<int> dp(n + 1, INT_MAX);
         dp.at(0) = 0;
-        for (int i = 0; i < sq_num.size() && sq_num.at(i) <= n; i++) {
-            for (int j = sq_num.at(i); j <= n; j++) {
-                dp.at(j) = min(dp.at(j - sq_num.at(i)) + 1, dp.at(j));
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < 100; j++) {
+                tmp = j * j;
+                if (tmp > i)
+                    break;
+                dp.at(i) = min(dp.at(i - tmp) + 1, dp.at(i));
             }
         }
         return dp.at(n);
